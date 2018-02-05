896ec9ab907b0dab31d3fa426a5bdd9622c69008
Tom Hazledine <tom.hazledine@creation.agency>
2018-02-05 17:12:40 +0000

    6: redux store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 export const counter = (state = 0, action) => {
     switch (action.type) {
         case 'INCREMENT':
@@ -8,3 +10,16 @@ export const counter = (state = 0, action) => {
             return state;
     }
 };
+
+const store = createStore(counter);
+
+const render = () => {
+    document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' });
+});
