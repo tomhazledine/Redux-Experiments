101a5104e8dbd25239b752f69bab42eec678a338
Tom Hazledine <tom.hazledine@creation.agency>
2018-02-05 17:31:10 +0000

    testing immutability with deep freeze

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
 
-export const counter = (state = 0, action) => {
+const counter = (state = 0, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return state + 1;
@@ -36,3 +38,25 @@ const render = () => {
 
 store.subscribe(render);
 render();
+
+expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+expect(counter(1, { type: 'SOMETHING' })).toEqual(1);
+expect(counter(undefined, {})).toEqual(0);
+
+const addCounter = list => {
+    list.push(0);
+    return list;
+};
+
+const testAddCounter = () => {
+    const listBefore = [];
+    const listAfter = [0];
+
+    deepFreeze(listBefore);
+
+    expect(addCounter(listBefore)).toEqual(listAfter);
+};
+
+testAddCounter();
