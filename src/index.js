db9dd330057b2526cbbaa7d8d2322fe891bf34cc
Tom Hazledine <tom.hazledine@creation.agency>
2018-02-05 17:43:35 +0000

    immutable array operations

--- src/index.js
+++ src/index.js
@@ -46,8 +46,15 @@ expect(counter(1, { type: 'SOMETHING' })).toEqual(1);
 expect(counter(undefined, {})).toEqual(0);
 
 const addCounter = list => {
-    list.push(0);
-    return list;
+    return [...list, 0];
+};
+
+const removeCounter = (list, index) => {
+    return [...list.slice(0, index), ...list.slice(index + 1)];
+};
+
+const incrementCounter = (list, index) => {
+    return [...list.slice(0, index), list[index] + 1, ...list.slice(index + 1)];
 };
 
 const testAddCounter = () => {
@@ -59,4 +66,24 @@ const testAddCounter = () => {
     expect(addCounter(listBefore)).toEqual(listAfter);
 };
 
+const testRemoveCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+
+    deepFreeze(listBefore);
+
+    expect(removeCounter(listBefore, 1)).toEqual(listAfter);
+};
+
+const testIncrementCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 11, 20];
+
+    deepFreeze(listBefore);
+
+    expect(incrementCounter(listBefore, 1)).toEqual(listAfter);
+};
+
 testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
