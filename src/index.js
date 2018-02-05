0701b8531726848a36b31c05ee35c2df3bd56db4
Tom Hazledine <tom.hazledine@creation.agency>
2018-02-05 17:03:17 +0000

    es6 counter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-export function counter(state, action) {
-    if (typeof state === 'undefined') {
-        return 0;
+export const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
     }
-
-    if (action.type === 'INCREMENT') {
-        return state + 1;
-    } else if (action.type === 'DECREMENT') {
-        return state - 1;
-    } else {
-        return state;
-    }
-}
+};
