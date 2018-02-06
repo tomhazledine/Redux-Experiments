7d5125755a736cf1f473fc4e51dd0d20a6be39f1
Tom Hazledine <tom.hazledine@creation.agency>
2018-02-06 11:13:01 +0000

    17: adding react to render app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 
 // Todo reducer (called by Todos reducer)
@@ -47,11 +47,46 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
     }
 };
 
-// Combined reducer
+// Combined reducer (state keys match the names of the reducers,
+// so we can write `todos,` rather than `todos: todos,`)
 const todoApp = combineReducers({
     todos,
     visibilityFilter
 });
 
 const store = createStore(todoApp);
-console.log(store.getState());
+
+let nextTodoId = 0;
+class TodoApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <input
+                    ref={node => {
+                        this.input = node;
+                    }}
+                />
+                <button
+                    onClick={() => {
+                        store.dispatch({
+                            type: 'ADD_TODO',
+                            text: this.input.value,
+                            id: nextTodoId++
+                        });
+                        this.input.value = '';
+                    }}
+                >
+                    Add Todo
+                </button>
+                <ul>{this.props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}</ul>
+            </div>
+        );
+    }
+}
+
+const render = () => {
+    ReactDOM.render(<TodoApp todos={store.getState().todos} />, document.getElementById('root'));
+};
+
+store.subscribe(render);
+render();
